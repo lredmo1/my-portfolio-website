0c2711f81c55e0dcb5445bae27d2babd96ed6bee
Lisa <redlisa125@gmail.com>
2022-02-07 13:42:08 -1000

    initial components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
-import ArtContainer from 'ArtContainer'
-import BakingContainer from 'BakingContainer'
-import CodeContainer from 'CodeContainer'
+import Home from './Home'
+import ArtContainer from './ArtContainer'
+import BakingContainer from './BakingContainer'
+import CodeContainer from './CodeContainer'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Home/>
+        <Routes>
+          <Route exact path="/" element={ <Home />} >
+            {/* <Home/> */}
           </Route>
           <Route exact path="/art">
             <ArtContainer/>
@@ -21,7 +22,7 @@ function App() {
           <Route exact path="/code">
             <CodeContainer/>
           </Route>
-        </Switch>
+        </Routes>
       </BrowserRouter>
     </div>
   );
