626878bcb40c16d16a19d03ee1ed916be5c4a914
Lisa <redlisa125@gmail.com>
2022-02-08 15:29:36 -1000

    added styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,10 +4,16 @@ function Navbar() {
       
     return (
     <div id="navbar">
-        <Link to="/" className="nav-links" style={{textDecoration: 'none'}}> Home</Link>
-        <Link to="/art" className="nav-links" style={{textDecoration: 'none'}}> Art</Link>
-        <Link to="/baking" className="nav-links" style={{textDecoration: 'none'}}> Baking</Link>
-        <Link to="/code" className="nav-links" style={{textDecoration: 'none'}}> Code</Link>
+        <div id="navbar-title">    
+            <span>The ABC Creative</span>
+            {/* <span>By Lisa Primeaux-Redmond</span> */}
+        </div>
+        <div id="navbar-links">
+            <Link to="/" className="nav-links" style={{textDecoration: 'none'}}> Home</Link>
+            <Link to="/art" className="nav-links" style={{textDecoration: 'none'}}> Art</Link>
+            <Link to="/baking" className="nav-links" style={{textDecoration: 'none'}}> Baking</Link>
+            <Link to="/code" className="nav-links" style={{textDecoration: 'none'}}> Code</Link>
+        </div>
     </div>
     )
 }
