8f15f01827d23cd6c35ce125f7860cb51c9f2d98
Lisa <redlisa125@gmail.com>
2022-02-09 10:05:14 -1000

    added homepage hover captions

--- src/Home.js
+++ src/Home.js
@@ -3,26 +3,34 @@ import baking_thumbnail from "./images/baking_images/Cake_Portfolio_Thumbnail.pn
 import code_thumbnail from "./images/coding_images/Coding_Portfolio_Thumbnail.jpg";
 
 function Home() {
-    return (
+  return (
     <>
-        <h1 id="home-title">The ABC Creative</h1>
-        <h2>A Digital Portfolio of Creations by Lisa Primeaux-Redmond</h2>
-        <div id="home-thumbnails">
-            <div>
-                <a href="/art"><img src={art_thumbnail} className="home-thumbnails"/></a>
-                <p className="home-thumbnail-title">ART</p>
-            </div>
-            <div>
-                <a href="/baking"><img src={baking_thumbnail} className="home-thumbnails"/></a>
-                <p className="home-thumbnail-title">Baking</p>
-            </div>
-            <div>
-                <a href="/code"><img src={code_thumbnail} className="home-thumbnails"/></a>
-                <p className="home-thumbnail-title">Code</p>
-            </div>      
+      <h1 id="home-title">The ABC Creative</h1>
+      <h2>A Digital Portfolio of Creations by Lisa Primeaux-Redmond</h2>
+      <div id="home-thumbnails">
+        <div className="item-wrapper">
+          <div className="img__wrap-home">
+            <a href="/art">
+              <img src={art_thumbnail} className="item-home" />
+            </a>
+            <p class="img__description">ART</p>
+          </div>
+          <div className="img__wrap-home">
+            <a href="/baking">
+              <img src={baking_thumbnail} className="item-home" />
+            </a>
+            <p class="img__description">Baking</p>
+          </div>
+          <div className="img__wrap-home">
+            <a href="/code">
+              <img src={code_thumbnail} className="item-home" />
+            </a>
+            <p class="img__description">Code</p>
+          </div>
         </div>
+      </div>
     </>
-    )
+  );
 }
-    
+
 export default Home;
